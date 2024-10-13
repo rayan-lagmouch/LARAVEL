4b76972bfde0b29d7b5a2a3dd7c3201050984fe3
rayan-lagmouch <rayan@360dayz.com>
2024-10-13 23:02:34 +0200

    les 9

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,38 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'director',
-                'salary' => '50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'programmer',
-                'salary' => '10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'teacher',
-                'salary' => '40,000'
-            ]
-        ];
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
 
+    protected $fillable = ['title', 'salary'];
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] === (int) $id);
-
-        if (! $job){
-            abort (404);
-        }
-        return $job;
-    }
 }
