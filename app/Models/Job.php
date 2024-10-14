388e71c345dc81c3c5a7ebb322e8b628d9d71cd1
rayan-lagmouch <rayan@360dayz.com>
2024-10-14 10:10:30 +0200

    les11

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,10 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model {
+    use Hasfactory;
+
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
 }
