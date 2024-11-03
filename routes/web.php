36fb4e23793e1293f830f27a1a9e3cbeb332c179
rayan-lagmouch <rayan@360dayz.com>
2024-11-03 15:45:45 +0100

    les 18

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -15,31 +16,51 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        @@ -40,6 +44,41 @@
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+// Update
+Route::patch('/jobs/{id}', function ($id) {
     request()->validate([
         'title' => ['required', 'min:3'],
         'salary' => ['required']
     ]);
-
-    Job::create([
+    // authorize (On hold...)
+    $job = Job::findOrFail($id);
+    $job->update([
         'title' => request('title'),
         'salary' => request('salary'),
-        'employer_id' => 1
     ]);
-
+    return redirect('/jobs/' . $job->id);
+});
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (On hold...)
+    Job::findOrFail($id)->delete();
     return redirect('/jobs');
 });
-
 Route::get('/contact', function () {
     return view('contact');
 });
